9e4bb8b761fffa27ec8f22e40df9a9a125c21a35
mapoxi <mapoxi@gmail.com>
2014-01-05 05:27:39 +0100

    Lab01 completed

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab01/7.Podsumowanie.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab01/7.Podsumowanie.c
@@ -4,7 +4,7 @@ int main(){
 
 int i = 80;
 
-printf("Program wyswietli najpierw liczbe, a potem litere o takiej samej wartosci kodu ASCII\nPowinno wyjsc 80 P\nA wiec sie przekonajmy: ");
+printf("Program wyswietla najpierw liczbe, a potem litere o takiej samej wartosci kodu ASCII\nPowinno wyjsc 80 P\nA wiec sie przekonajmy: ");
 printf("%i %c\n",i,i);
 
 return 0;
