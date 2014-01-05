fbe8b863b8ed1b6b89d233482b73deeabe592ecd
mapoxi <mapoxi@gmail.com>
2014-01-05 20:46:35 +0100

    0604 progress

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab06/4.Palindromy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+	char tablica[100];
+	int licznik = 0;
+	char litera = a;
+	
+	printf("Wpisz s³owo, ktore chcesz sprawdzic. Zakoncz Enterem: ");
+	
+	while ((int)litera != 10){
+		scanf("%c", &litera);
+		if ((int)litera != 10) tablica[i] = litera;
+		i = i+1;
+		}
+		
+		
+
+
+
+	return 0;
+	}
