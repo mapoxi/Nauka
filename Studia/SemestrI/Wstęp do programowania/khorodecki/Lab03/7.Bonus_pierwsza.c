cc3c0b4c2562f087874e5401d2ebd618f178f34b
mapoxi <mapoxi@gmail.com>
2014-01-05 02:22:19 +0100

    Lab03/Zad bonusowe

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab03/7.Bonus_pierwsza.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    int liczba;
+    int licznik;
+    int tester = 0;
+    
+    
+    printf("Podaj liczbe, ktora chcesz sprawdzic: ");
+    scanf("%d",&liczba);
+    
+    licznik = liczba;
+    
+    while (licznik>1){
+          if(licznik%liczba == 0) tester = tester + 1;
+          licznik = licznik-1;
+          }
+          
+    if (tester == 1) printf("\nLiczba %d jest pierwsza\n",liczba);
+    else printf("Liczba nie jest pierwsza\n");     
+    
+    return 0;
+}
