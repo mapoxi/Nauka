c60c2dab05ad139c43775a61377e2d12756c4c43
Piotr Młynarski <mapoxi@gmail.com>
2014-01-05 20:38:58 +0100

    0603 completed finally

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab06/3.Tablice_we_wy.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab06/3.Tablice_we_wy.c
@@ -8,14 +8,14 @@ int main(){
 	
 	printf("Podaj liczby do wrzucenia do tablicy (nie wiecej niz 50)\n");
 	
-	while (i<50 && warunek>0){
+	while (i<50 && warunek >= 0){
 		scanf("%d",&warunek);
 		tablica[i] = warunek;
 		if(warunek >= 0) i = i+1;
 		}
 		
-	while(i>=0){
-		printf("%d ",tablica[i]);
+	while(i>0){
+		printf("%d ",tablica[i-1]);
 		i = i-1;
 		}
 	
