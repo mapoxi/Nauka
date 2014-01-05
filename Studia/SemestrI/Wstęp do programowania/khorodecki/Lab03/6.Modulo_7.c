a8bfa2ca87b1f225065e3f0381e6aad229e5d719
mapoxi <mapoxi@gmail.com>
2014-01-05 02:03:23 +0100

    Lab03/Zad 6

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab03/6.Modulo_7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+	int suma;
+	int i = 21;
+	int licznik;
+	
+	while (licznik < 50){
+		i = i+7;
+		printf ("%d ",i);
+		licznik = licznik+1;
+		}
+	printf("\n%d modulo 7 = %d, wiec wszystko jest ok :)\n",suma,suma%7);
+	
+
+
+return 0;
+}
