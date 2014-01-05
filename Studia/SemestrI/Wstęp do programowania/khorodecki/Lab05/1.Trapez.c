da55a286f10398f3cab917f4103475c6cde10639
mapoxi <mapoxi@gmail.com>
2014-01-05 05:55:28 +0100

    Lab05/Zad 1 completed

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab05/1.Trapez.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+int a,h;
+int petla = 0;
+int i = 0;
+
+printf("Program rysuje trapez prostokatny. Podaj wysokosc oraz dlugosc gornej podstawy:\n");
+scanf("%d%d",&h,&a);
+printf("\n");
+
+while(petla < h){
+	while(i < a){
+		printf("*");
+		i = i + 1;
+		}
+	printf("\n");
+	i = 0;
+	a = a+1;
+	petla = petla+1;
+
+return 0;
+}
