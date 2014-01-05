aee8314891e447f83a58ff1a1973668a023d1a6e
mapoxi <mapoxi@gmail.com>
2014-01-05 20:56:11 +0100

    0604

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab06/4.Palindromy.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab06/4.Palindromy.c
@@ -5,18 +5,28 @@ int main(){
 	char tablica[100];
 	int i = 0;
 	char litera = 'a';
+	int pocz, kon;
+	int warunek = 0;
 	
 	printf("Wpisz slowo, ktore chcesz sprawdzic. Zakoncz Enterem: ");
 	
 	while ((int)litera != 10){
 		scanf("%c", &litera);
-		if ((int)litera != 10) tablica[i] = litera;
-		i = i+1;
+		if ((int)litera != 10){
+			tablica[i] = litera;
+			i = i+1;
+			}
+		}
+	
+	pocz = 0;
+	kon = i;
+	
+	while (koniec <=0 && warunek == 0){
+		if(tablica[pocz] != tablica[kon]) warunek = 1;
+		pocz = pocz+1;
+		kon = kon-1;
 		}
 		
-		
-
-
 
 	return 0;
 	}
