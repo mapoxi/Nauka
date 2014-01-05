b40ac25bef42dbf3a8829e396407888e95f095c0
mapoxi <mapoxi@gmail.com>
2014-01-05 05:07:29 +0100

    Lab04/Zad 3 wykończenie

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab04/3.Trojkat.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab04/3.Trojkat.c
@@ -2,20 +2,47 @@
 
 int main(){
     
-    int n;
+    int wejscie;
     int h = 0;
     int licznik = 0;
+    int spacja;
+	int wiersz = 1;
+	int wykonane = 1;
     
     
     printf("Podaj z ilu liczb ma sie skladac trojkat: ");
-    scanf("%d",&n);
+    scanf("%d",&wejscie);
     
-    while (licznik < n){
+    while (licznik < wejscie){
           h = h+1;
           licznik = licznik + h;
           }
+    
+	licznik = 0;
+	
+    while (h>0){
+          spacja = (h-1)*3/2;
           
-          printf("\n%d\n",h);
+        while (spacja>0){
+                printf(" ");
+                spacja = spacja-1;
+                }
+        while (licznik < wiersz || wykonane <=wejscie){
+			printf("%3d",wykonane);
+			licznik = licznik+1;
+			wykonane = wykonane+1;
+			}
+			
+		spacja = (h-1)*3/2;
+          
+        while (spacja>0){
+                printf(" ");
+                spacja = spacja-1;
+                }
+		printf("\n");
+		h = h+1;
+          
+          }
     
     
     
