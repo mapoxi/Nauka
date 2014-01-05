79ec6e180237ee6c87569663a4f6f4f6d0519ede
mapoxi <mapoxi@gmail.com>
2014-01-05 20:06:01 +0100

    Lab06/Zad 1 completed

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab06/1.Najwieksza.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a,b,c,najwieksza;
+    
+    printf("Podaj 3 cyfry do sprawdzenia: ");
+    scanf("%d%d%d",&a,&b,&c);
+    
+    najwieksza = a;
+    if(b<a) najwieksza = b;
+    if(c>najwieksza) najwieksza = c;
+    
+    printf("\nNajwieksza liczba to: %d\n",najwieksza);  
+    
+    
+    return 0;
+}
