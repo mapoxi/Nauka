6cd4da785e5acb9c7b9963f154f0d88cd6f8c307
mapoxi <mapoxi@gmail.com>
2014-01-05 06:28:03 +0100

    Lab05/Zad 4

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab05/4.Doskonale.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	int licznik;
+	int suma;
+	int liczba = 3;
+	int dzielna;
+	
+	printf("Program drukuje 4 pierwsze liczby doskonale.\nOto one: ");
+	
+	while(licznik < 4){
+		suma = 0;
+		dzielna = liczba-1;
+		while (dzielna>0){
+			if(liczba%dzielna == 0) suma = suma+dzielna;
+			dzielna = dzielna-1;	
+			}
+
+		if(liczba == suma){
+			prinf("%d\n",liczba);
+			licznik = licznik+1;
+			}
+			
+		liczba = liczba+1;
+	}
+	
+return 0;
+}
