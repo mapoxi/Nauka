bf65c6b6db764a2611ed3293cd31f8126b66c09c
mapoxi <mapoxi@gmail.com>
2014-01-06 22:54:40 +0100

    0702

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab07/2.Iloczyn_skalarny.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+
+	int a[10];
+	int b[10];
+	int i=0;
+	
+	printf("Podaj 10 liczb do tablicy a: );
+	
+	while (i < 10){
+		scanf("%d",&a[i];
+		i = i+;
+		}
+		
+	printf("Podaj 10 liczb do tablicy b: );
+	
+	i = 0;
+	while (i < 10){
+		scanf("%d",&b[i];
+		i = i+;
+		}
+	
+	i = 0;
+	while (i < 10){
+		a[i] = a[i]*b[i];
+		i = i+1;
+		}
+	
+	printf("\nIloczyn skalarny to:\n");
+	
+	i = 0;
+	while (i < 10){
+		printf("%d. %d\n",i+1,a[i]);
+		i = i+1;
+		}
+		
+	return 0;
+	}
