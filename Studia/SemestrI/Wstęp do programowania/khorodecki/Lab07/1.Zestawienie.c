eb3d5eb53874d6d1a377d3eb1bf85575631ca6c1
mapoxi <mapoxi@gmail.com>
2014-01-06 20:50:36 +0100

    0701

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab07/1.Zestawienie.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab07/1.Zestawienie.c
@@ -4,15 +4,15 @@ int main(){
 
 	float tab[100];
 	int i = 0;
-	int wejscie = 0;
+	int wejscie = (int)'\n';
 	
-	printf("Wpisz liczby to zestawienia:\n");
+	printf("Wpisz liczby to zestawienia: %d\n",wejscie);
 	
-	while(wejscie != (int)'\n'){
+	/*while(wejscie != (int)'\n'){
 		scanf("%f",&wejscie);
 		tab[i] = wejscie;
 		i = i+1;
-		}
+		}*/
  
 	return 0;
 	}
