ce7f835c1289ca5732cf7430dcbb396b67a45973
mapoxi <mapoxi@gmail.com>
2014-01-06 20:13:39 +0100

    0701

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab07/1.Zestawienie.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+	//float tab[100];
+	//int licznik;
+	int wejscie = '7';
+	
+	printf("Wpisz liczby to destawienia:\n");
+	
+	printf("%d", wejscie);
+
+
+
+
+	return 0;
+	}
