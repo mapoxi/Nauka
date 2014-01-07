ab518c419f01a14664f00c800c3307f1df371037
Piotr Młynarski <mapoxi@gmail.com>
2014-01-07 01:34:36 +0100

    0801 completed

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/1.Duze_dodawanie.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/1.Duze_dodawanie.c
@@ -6,15 +6,15 @@ int main(){
 	int i;
 	
 	printf("Podaj pierwsza 10 cyfrowa liczbe: ");
-	for(i=0; i<10; i = i+1) tablica[0][i] = getchar();
-	
+	for(i=0; i<10; i = i+1) tablica[0][i] = getchar()-48;
+	getchar();
 	printf("\nPodaj druga liczbe: ");
-	for(i=0; i<10; i = i+1) tablica[1][i] = getchar();
-	
-	for(i=9; i>=0; i = i-1) tablica[0][i] = tablica[0][i]+tablica[1];
+	for(i=0; i<10; i = i+1) tablica[1][i] = getchar()-48;
+
+	for(i=9; i>=0; i = i-1) tablica[0][i] = tablica[0][i]+tablica[1][i];
 	
 	for(i=9; i>0; i = i-1)
-		if(tablica[0][i]>=0){
+		if(tablica[0][i]>=10){
 			tablica[0][i] = tablica[0][i]-10;
 			tablica[0][i-1] = tablica[0][i-1]+1;
 			}
@@ -23,7 +23,7 @@ int main(){
 		tablica[0][0] = tablica[0][0]-10;
 		printf("\n1");
 		}
-	for(i=1; i < 10; i = i+1) printf("%d",tablica[0][i]);
+	for(i=0; i < 10; i = i+1) printf("%d",tablica[0][i]);
 	printf("\n");
 
 	return 0;
