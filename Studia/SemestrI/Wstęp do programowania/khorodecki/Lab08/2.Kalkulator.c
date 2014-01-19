9cb4abfb4adf60a4fd74d85fed3bcbf8a0f4aac1
mapoxi <mapoxi@gmail.com>
2014-01-19 20:30:31 +0100

    0802 Kalkulator

---
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/2.Kalkulator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i = 10
+	
+	while(i != 0){
+		printf("wybierz dzialanie:\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n5. Exit\n");
+		scanf("%d",&i);
+		printf("Wybrano: %d",i);
+	}
+
+
+return 0;
+}
