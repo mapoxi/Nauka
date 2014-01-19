b15d844a10a60ae0b165b49e9593529f0a5abb06
mapoxi <mapoxi@gmail.com>
2014-01-19 20:35:30 +0100

    0802

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/2.Kalkulator.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/2.Kalkulator.c
@@ -4,10 +4,10 @@ int main(){
 	
 	int i = 10;
 	
-	while(i != 0){
+	while(i != 5){
 		printf("wybierz dzialanie:\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n5. Exit\n");
 		scanf("%d",&i);
-		printf("Wybrano: %d",i);
+		printf("Wybrano: %d\n",i);
 	}
 
 
