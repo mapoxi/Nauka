7e68fb681724406e3d269183cadbe427ee1cd9a5
mapoxi <mapoxi@gmail.com>
2014-01-19 22:05:15 +0100

    0802

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/2.Kalkulator.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/2.Kalkulator.c
@@ -2,12 +2,22 @@
 
 int tablica[3][100];
 
+void czysc(){
+	int i = 0;
+	int j = 0;
+	
+	for(i=0; i<3; i=i+1)
+		for(j=0; j<100; j=j+1)
+			tablica[i][j] = 0;
+}
+
 int main(){
 	
 	char wejscie;
 	int i = 10;
 	
 	while(i != 5){
+		czysc();
 		printf("wybierz dzialanie:\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n5. Exit\n");
 		scanf("%d",&i);
 		printf("Wybrano: %d\n",i);
