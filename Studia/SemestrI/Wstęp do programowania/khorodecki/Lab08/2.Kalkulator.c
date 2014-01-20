c4d0f7e562c5f5f71337c80282c66f5c1357209a
mapoxi <mapoxi@gmail.com>
2014-01-20 01:15:56 +0100

    0802

--- Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/2.Kalkulator.c
+++ Studia/SemestrI/Wstęp do programowania/khorodecki/Lab08/2.Kalkulator.c
@@ -48,8 +48,9 @@ void wejscie(int liczba){
 void dodawanie(){	
 	printf("Wybrano dodawanie dwoch duzych liczb.\n");
 	wejscie(0);
-	printf("%d%d%d%d%d\n",tablica[0][0],tablica[0][1],tablica[0][2],tablica[0][3],tablica[0][4]);
-	printf("Znak: %d, dlugosc: %d\n",znak[0], dlugosc[0]);
+	wejscie(1);
+	printf("%d%d%d%d%d\n",tablica[1][0],tablica[1][1],tablica[1][2],tablica[1][3],tablica[1][4]);
+	printf("Znak: %d, dlugosc: %d\n",znak[1], dlugosc[1]);
 	getchar();
 }
 
@@ -71,7 +72,7 @@ int main(){
 	
 	int sterownik = 0;
 	while(sterownik != 5){
-		printf("wybierz dzialanie:\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n5. Exit\n");
+		printf("\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n5. Exit\nWybierz dzialanie: ");
 		scanf("%d",&sterownik);
 		if(sterownik != 5) menu(sterownik);
 	}
